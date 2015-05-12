45bb9b563f6e1a4906d334840251a5c7510f3911
Giovanni Collazo <hello@gcollazo.com>
2015-05-12 16:55:27 -0400

    Rename mixin

--- addon/mixins/authenticated-route-mixin.js
+++ addon/mixins/authenticated-route-mixin.js
@@ -1,10 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
-  beforeModel() {
+  beforeModel(transition) {
+    var superResult = this._super(transition);
+
     if (!this.get('session.isAuthenticated')) {
+      transition.abort();
       var config = this.container.lookupFactory('config:environment');
       this.transitionTo(config['ember-parse'].session.loginRoute);
     }
+
+    return superResult;
   }
 });
