a3caca12d3f6669ad1d08b1628fd12e8191338b4
Giovanni Collazo <hello@gcollazo.com>
2015-06-06 21:33:05 -0400

    Removes upgrade ember cli script

--- upgrade-ember-cli.sh
+++
@@ -1,9 +0,0 @@
-npm uninstall -g ember-cli
-npm cache clean
-bower cache clean
-npm install -g ember-cli@0.2.7
-npm install ember-cli@0.2.7 --save-dev
-rm -rf node_modules bower_components dist tmp
-npm install
-bower install
-ember init
